a86dc2cabc3b08835f35664fd99f5a4fbedf6156
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-03-20 18:19:57 +0100

    fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+
+
 const age = prompt('Quanti anni hai?');
 
 console.log(age);
+
+const distance = prompt('Quanti km percorri');
+
+console.log(distance);
+
+let price = distance * 0.21;
+
+console.log(price);
+
+let discount = 0;
+
+if (age < 18){ 
+  discount = price * 0.2; 
+}
+else if (age > 65){
+  discount = price * 0.4;
+}
+
+price = price - discount;
+
+
+const message = `
+  Il prezzo del biglietto e $(discount)
+`
+
+document.getElementById('discount').innerHTML = discount;

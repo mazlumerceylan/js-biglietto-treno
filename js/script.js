78baf7ebf42d2c83fd9b67b3ecca5d710ecdc187
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-03-27 22:29:52 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,32 +1,73 @@
 
 
-const age = prompt('Quanti anni hai?');
 
-console.log(age);
+const inputUser = document.getElementById('name');
+const inputKm = document.getElementById('km');
+const selectEta = document.getElementById('fasciaEta');
 
-const distance = prompt('Quanti km percorri');
 
-console.log(distance);
+const btnGenera = document.getElementById('genera');
+const btnAnnulla = document.getElementById('annulla');
 
-let price = distance * 0.21;
 
-console.log(price);
+btnGenera.addEventListener('click', function(){
 
-let discount = 0;
+  let validForm = true;
+  const nameUser = inputUser.value;
+  const km = parseInt(inputKm.value);
+  const fasciaEta = selectEta.value;
+  const prezzoKm = 0.21;
+  const carrozza = Math.ceil(Math.random() * 3);
+  const codiceCP = Math.floor(Math.random() * (99000 - 10000 + 1) ) + 10000;
 
-if (age < 18){ 
-  discount = price * 0.2; 
-}
-else if (age > 65){
-  discount = price * 0.4;
-}
+  let offerta = 'Prezzo Standard',
+      sconto = 0,
+      prezzo;
 
-price = price - discount;
+  if(fasciaEta === 'minorenne'){
+    sconto = 20;
+    offerta = 'Sconto Minorenni';
+  }else if(fasciaEta === 'senior'){
+     sconto = 40;  
+     offerta = 'Sconto Silver';
+  }
 
-discount = (Math.round(price * 100) / 100 );
+  prezzo = (km * prezzoKm) * (1 - sconto/100);
 
-const message = `
-  Il prezzo del biglietto e $(discount)
-`
+  if(nameUser.length < 3) validForm = false;
 
-document.getElementById('discount').innerHTML = discount;
+  if(isNaN(km)) validForm = false;
+  
+
+  if(fasciaEta === '') validForm = false;
+
+  const outputName =      document.getElementById('outputName');
+  const outputOfferta =   document.getElementById('outputOfferta');
+  const outputCarrozza =  document.getElementById('outputCarrozza');
+  const outputCp =        document.getElementById('outputCp');
+  const outputPrezzo =    document.getElementById('outputPrezzo');
+
+
+  if(validForm){
+    outputName.innerHTML =      nameUser;
+    outputOfferta.innerHTML =   offerta;
+    outputCarrozza.innerHTML =  carrozza;
+    outputCp.innerHTML =        codiceCP;
+    outputPrezzo.innerHTML = `
+    &euro; ${prezzo.toFixed(2)}
+    `;
+    document.querySelector('.ticket-area').classList.remove('hide');
+  }else{
+    alert('Inserire tutti i campi')
+  }
+});
+
+
+
+
+btnAnnulla.addEventListener('click', function(){
+  inputUser.value = '';
+  inputKm.value = '';
+  selectEta.value = '';
+  document.querySelector('.ticket-area').classList.add('hide');
+})
